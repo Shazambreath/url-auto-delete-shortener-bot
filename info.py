1e259fea52f0550cc1068e6629324157dab3f768
Vignesh suresh <133358410+Shazambreath@users.noreply.github.com>
2023-05-26 17:20:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,12 +35,12 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vigneshs:1234567890@cluster0.t8jmjdh.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster00")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shazambreath58:1234567890@cluster58.lmmu75x.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster58")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001854924506'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001983684568'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'search_zone_support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
